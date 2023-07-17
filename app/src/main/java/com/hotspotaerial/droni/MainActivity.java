d14d7470dad25ffad351a9940e4453bed0b6e9ae
sila <silakironji@gmail.com>
2023-07-17 09:33:06 +0300

    Initial commit

---
+++ app/src/main/java/com/hotspotaerial/droni/MainActivity.java
@@ -0,0 +1,39 @@
+package com.hotspotaerial.droni;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton droneControlScreen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setNavigationBarColor(Color.parseColor("#000000"));
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            getWindow().setStatusBarColor(Color.parseColor("#000000"));
+        }
+
+        droneControlScreen = findViewById(R.id.introImage);
+        droneControlScreen.setOnClickListener(v -> {
+            Intent droneControlScreenIntent = new Intent(MainActivity.this, droneController.class);
+            startActivity(droneControlScreenIntent);
+        });
+
+
+    }
+}
